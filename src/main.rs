d24a59997fcfc37de57a07b2c6a0ff8fe6f3d864
forks <redforks@gmail.com>
2025-03-02 10:53:49 +0800

    feat: Allow passing arguments to the executed program

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 use clap::Parser;
 use notify_rust::Notification;
 use std::error::Error;
+use std::process::Stdio;
+use std::sync::Arc;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::process::Command;
 use tokio::sync::Mutex;
-use std::process::Stdio;
-use std::sync::Arc;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+#[command(trailing_var_arg = true)]
 struct Args {
     /// The program to execute
     #[arg()]
     program: String,
+
+    /// Arguments to pass to the program
+    #[arg(trailing_var_arg = true)]
+    args: Vec<String>,
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -20,6 +25,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     let mut command = Command::new(&args.program);
+    command.args(&args.args);
     command.stdout(Stdio::piped());
     command.stderr(Stdio::piped());
 
@@ -28,12 +34,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // Capture stderr in real-time
     let stderr = child.stderr.take().expect("Failed to capture stderr");
     let stderr_lines = Arc::new(Mutex::new(Vec::new()));
-    
+
     // Process stderr asynchronously
     let stderr_lines_clone = Arc::clone(&stderr_lines);
     let stderr_reader = async move {
         let mut reader = BufReader::new(stderr).lines();
-        
+
         while let Some(line) = reader.next_line().await.expect("Failed to read line") {
             // Print to stderr immediately
             eprintln!("{}", line);
@@ -49,11 +55,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let stdout_reader = async {
         let mut reader = BufReader::new(stdout).lines();
         let mut stdout_content = Vec::new();
-        
+
         while let Some(line) = reader.next_line().await.expect("Failed to read line") {
             stdout_content.push(line);
         }
-        
+
         stdout_content.join("\n")
     };
 
